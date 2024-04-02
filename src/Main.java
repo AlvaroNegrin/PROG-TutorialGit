94c5c9152858738001ea568bafa5caee9b6bb29a
AlvaroNegrin <usr3856@salesianos-lacuesta.net>
2024-04-02 12:20:58 +0100

    feat(Main): ask number to user

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        final Scanner SC = new Scanner(System.in);
+        System.out.println("Dame un número");
+        int num = SC.nextInt();
+        System.out.println("Tu número es " + num);
+
+        SC.close();
     }
 
 }
